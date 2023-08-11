d7fd5cb42747255cc93ce65fed6df39357586007
Kazte <39470734+Kazte@users.noreply.github.com>
2023-08-11 11:51:00 -0300

    [feat] - providers

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -3,7 +3,13 @@
 
 import { store } from "@/store/store";
 import { Provider } from "react-redux";
+import { NextUIProvider } from "@nextui-org/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-	return <Provider store={store}>{children}</Provider>;
+	return <Provider store={store}>
+		<NextUIProvider>
+			{children}
+		</NextUIProvider>
+
+	</Provider>;
 }
