60f693e2db52b86c18165fbf440d53827b36c487
Kazte <39470734+Kazte@users.noreply.github.com>
2023-08-26 21:41:56 -0300

    test

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -3,66 +3,69 @@ import { UserInfo } from "../../models/user.model";
 
 const baseUrlAuth = API_BASE_URL + '/auth';
 
+
+
 export interface AuthRepository {
-	login: (email: string, password: string) => Promise<AuthResponse>;
-	register: (username: string, email: string, password: string) => Promise<AuthResponse>;
-	verify: (token: string) => Promise<AuthResponse>;
+  login: (email: string, password: string) => Promise<AuthResponse>;
+  register: (username: string, email: string, password: string) => Promise<AuthResponse>;
+  verify: (token: string) => Promise<AuthResponse>;
 }
 
 export type AuthResponse = {
-	result: boolean;
-	message: string;
-	data: {
-		user: UserInfo;
-		token: string;
-	}
+  result: boolean;
+  message: string;
+  data: {
+    user: UserInfo;
+    token: string;
+  }
 }
 
 class AuthService implements AuthRepository {
-	async login(email: string, password: string): Promise<AuthResponse> {
-		const response = await fetch(`${baseUrlAuth}/login`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ email, password })
-		});
-		const result = await response.json();
-		return result;
-	}
+  async login(email: string, password: string): Promise<AuthResponse> {
+    const response = await fetch(`${baseUrlAuth}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password })
+    });
+    const result = await response.json();
+    return result;
+  }
 
-	async register(username: string, email: string, password: string): Promise<AuthResponse> {
-		const response = await fetch(`${baseUrlAuth}/register`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ username, email, password })
-		});
-		const result = await response.json();
-		return result;
-	}
+  async register(username: string, email: string, password: string): Promise<AuthResponse> {
+    const response = await fetch(`${baseUrlAuth}/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, email, password })
+    });
+    const result = await response.json();
+    return result;
+  }
 
-	async verify(token: string): Promise<AuthResponse> {
-		const response = await fetch(`${baseUrlAuth}/verify`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'Authorization': `Bearer ${token}`
-			},
-		});
-		const result = await response.json();
-		return result;
-	}
+  async verify(token: string): Promise<AuthResponse> {
+    console.log(baseUrlAuth);
+    const response = await fetch(`${baseUrlAuth}/verify`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+    });
+    const result = await response.json();
+    return result;
+  }
 }
 
 let authService: AuthRepository | null = null;
 
 function createOrTake(): AuthRepository {
-	if (authService === null) {
-		authService = new AuthService();
-	}
-	return authService;
+  if (authService === null) {
+    authService = new AuthService();
+  }
+  return authService;
 }
 
 export const authServiceInstance = createOrTake();
